4cca453e9399d5b6bd7d89468fbdcddb1b97ebfc
marcosvilhena87 <105467394+marcosvilhena87@users.noreply.github.com>
2025-12-28 22:15:32 -0300

    Delete scripts directory

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Convenience imports for the library functions."""
-
-from .preprocess_data import process
-from .predict_results import predict
-from .pulverization import train_pulverization_models
-from .train_model import train
-
-__all__ = ["process", "train", "predict", "train_pulverization_models"]
